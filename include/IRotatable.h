43f2e36816188aad7fb9ba391a352a3cd7a303b9
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-02-01 23:41:57 +0500

    Create IRotatable.h

---
+++ include/IRotatable.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "object.h"
+
+class IRotatable
+{
+public:
+    virtual bool getAngular(object *obj, int dt) = 0;
+    virtual bool setAngular(object *obj) = 0;
+    virtual bool getAngularVelocity(object *obj, int dv) = 0;
+    virtual bool setAngularVelocity(object *obj) = 0;
+};
