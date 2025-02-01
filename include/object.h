31fd5af73dd47cb25b0d2b7c6e2aeae4ac358e84
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-02-01 23:45:13 +0500

    Create object.h

---
+++ include/object.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <vector>
+#include "minor.h"
+#include "IMoving.h"
+#include "IRotatable.h"
+
+class object : public IMoving, IRotatable
+{
+public:
+    object(int id, react state, coord place);
+    ~object();
+
+    int id() const;
+    react state() const;
+    coord place() const;
+
+    void setState(react state);
+    void setPlace(coord place);
+
+    bool getPosition(object *obj, int dt);
+    bool setPosition(object *obj);
+    bool getVelocity(object *obj, int du);
+    bool setVelocity(object *obj);
+
+    bool getAngular(object *obj, int dt);
+    bool setAngular(object *obj);
+    bool getAngularVelocity(object *obj, int dv);
+    bool setAngularVelocity(object *obj);
+
+    bool getFuel(object *obj, int dF);
+
+private:
+    class objectP* imp;
+};
+
+class objectVector
+{
+public:
+    objectVector();
+    ~objectVector();
+    void reset();
+
+    object* add(int id, react state, coord place);
+    bool isEmpty() const;
+    size_t count() const;
+    const std::vector<object *>& vector() const;
+    object* at(int i);
+private:
+    class objectVectorP* imp;
+};
