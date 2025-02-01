6a399fd5ab3271596cde09f0500193d1422b6879
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-02-01 23:44:08 +0500

    Create config.h

---
+++ include/config.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "CommandQueue.h"
+#include "CommandMove.h"
+#include "CommandRotate.h"
+#include "CommandLoger.h"
+#include "CommandRepeat.h"
+#include "ExceptionHandler.h"
+#include "CommandFuelCheck.h"
+#include "CommandFuelBurn.h"
+#include "CommandSimpleMacro.h"
