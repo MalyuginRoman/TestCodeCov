68955696310fa7d009283b149e4e02035196e81f
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-02-01 23:41:43 +0500

    Create IMoving.h

---
+++ include/IMoving.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class object;
+
+class IMoving
+{
+public:
+    virtual bool getPosition(object *obj, int dt) = 0;
+    virtual bool setPosition(object *obj) = 0;
+    virtual bool getVelocity(object *obj, int du) = 0;
+    virtual bool setVelocity(object *obj) = 0;
+};
