dda20db4e8c89beeb34f7967b3b9383630ea7f79
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-02-02 13:24:51 +0500

    Create queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "include/ioc.h"
+#include "include/icommand.h"
+#include "include/object.h"
+#include "include/producer.h"
+
+int main(int ac, char **av)
+{
+    std::cout << std::endl;
+    std::cout << "Testing thread methods..." << std::endl;
+#ifdef test1
+    test_thread1();
+#endif
+#ifdef test2
+    test_thread2();
+#endif
+    return 0;
+}
