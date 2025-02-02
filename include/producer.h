99c9ce11d502d70089224037c6e0671b1cd68f48
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-02-02 14:19:53 +0500

    Create producer.h

---
+++ include/producer.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <iostream>
+#include <thread>
+#include "ioc.h"
+#include "icommand.h"
+#include "object.h"
+#include "safequeue.h"
+#include "exceptionhandler.h"
+#include "eventloop.h"
+#include "istate.h"
+
+void test_thread1()
+{
+    std::cout << "Start test_thread1" << std::endl;
+    objectVector vector;
+    int count = 2;
+    for(int i = 0; i < count; i++)
+    {
+        int id = i;
+        coord place;
+        react state;
+
+        place.placeX = 0.;
+        place.placeY = 0.;
+        place.angular = 45 * i;
+
+        state.velocity = 100;
+        state.angularVelocity = 20;
+        state.fuel = 10;
+
+        vector.add(id, state, place);
+    }
+
+    for(int i = 0; i < count; i++)
+    {
+        std::cout << vector.at(i)->id() << ":" << vector.at(i)->state().velocity << "," << vector.at(i)->state().angularVelocity << "," << vector.at(i)->state().fuel
+                       << "," << vector.at(i)->place().placeX << "," << vector.at(i)->place().placeY << "," << vector.at(i)->place().angular << std::endl;
+    }
+
+    IocContainer<ICommand> ioc;
+    SafeQueue<ICommand*> queueCmds;
+    SafeQueue<ICommand*> queueCmds_1;
+
+    // Scope2 with fuel
+    // формируем макрокоманды
+    CheckCommand *cmd_check = new CheckCommand();
+    MoveCommand *cmd_move = new MoveCommand();
+    RotateCommand *cmd_rotate = new RotateCommand();
+    BurnCommand *cmd_burn = new BurnCommand();
+    EmptyCommand *cmd_empty = new EmptyCommand();
+  
+    queueCmds.push(cmd_check);
+    queueCmds.push(cmd_move);
+    queueCmds.push(cmd_burn);
+    queueCmds.push(cmd_check);
+    queueCmds.push(cmd_rotate);
+    queueCmds.push(cmd_burn);
+
+    StateStatus *sc = new StateStatus(new DefaultState(), cmd_empty);
+    
+    eventloop* producer = new eventloop(&queueCmds, sc);
+    producer->start(&queueCmds, sc, 1);
+}
