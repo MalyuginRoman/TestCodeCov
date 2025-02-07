b6ebbb5e813404674ffd77948993e8bb007e770d
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-02-07 16:15:33 +0500

    Create producer.h

---
+++ src/sec/producer.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "object.h"
+
+class producer
+{
+public:
+    void start_game();
+    void test_game(std::vector <char> message);
+    void read_message(std::vector <char> message);
+    void test_thread1();
+
+    objectVector vector_obj;
+    std::string gameId_s, objectId_s, operationId_s, args;
+    int gameId_i, objectId_i, operationId_i;
+};
