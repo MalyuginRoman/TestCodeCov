956b84efe0164638941931b6c382f4545634d73d
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-02-07 16:18:03 +0500

    Create ioc.h

---
+++ src/spe/ioc.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <map>
+#include <functional>
+#include <memory>
+#include "../com/icommand.h"
+
+template<class T>
+class IocContainer
+{
+public:
+    template<typename... Args>
+    std::shared_ptr<T> resolve(std::string key_f, std::map<std::string, std::function<ICommand*()>> m_map, std::map<std::string, std::string> m_scope, Args&&... arg)
+    {
+        if (m_map.find(key_f) == m_map.end())
+            throw runtime_error ("Unknown key_f");
+
+        int size = sizeof...(arg);
+        cout << "Argument number in resolved " << size << endl;
+
+        auto func = m_map[key_f];
+        return shared_ptr<T>(func());
+    }
+};
